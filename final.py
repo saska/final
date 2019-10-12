972eea04b0e48fa00814f39ddc71b2f612bf970a
Saska Karsi <saska.karsi@gmail.com>
2019-10-12 23:04:05 +0300

    Formatting

--- final.py
+++ final.py
@@ -1,10 +1,12 @@
 import numpy
 
 def bs_d1(sigma, time_to_maturity, rf_rate, spot, strike):
-    return 1 / (sigma * np.sqrt(time_to_maturity)) * (np.log(spot/strike) + (rf_rate + (sigma**2) / 2) * time_to_maturity)
+    return 1 / (sigma * np.sqrt(time_to_maturity)) \
+        * (np.log(spot/strike) + (rf_rate + (sigma**2) / 2) * time_to_maturity)
 
 def bs_d2(sigma, time_to_maturity, rf_rate, spot, strike): 
-    return bs_d1(sigma, time_to_maturity, rf_rate, spot, strike) - sigma * np.sqrt(time_to_maturity)
+    return bs_d1(sigma, time_to_maturity, rf_rate, spot, strike) \
+        - sigma * np.sqrt(time_to_maturity)
 
 def pv(strike, rf_rate, time_to_maturity):
     return strike * np.exp(-rf_rate * (time_to_maturity))
