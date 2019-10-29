94df5051c2afad8a632a8c4db5daae0144b5fd64
Saska Karsi <saska.karsi@gmail.com>
2019-10-30 00:27:40 +0200

    Docstrings for call and put

--- final.py
+++ final.py
@@ -13,10 +13,26 @@ def pv(strike, rate, time_to_maturity):
     return strike * np.exp(-rate * (time_to_maturity))
 
 def european_call_value(sigma, time_to_maturity, rf_rate, spot, strike):
+    """Black-Scholes formula for calculating European call option.
+
+    Args: 
+        sigma: volatility
+        time_to_maturity: T - t where T is exercise date and t is current (pricing) date
+        rf_rate: risk-free rate
+        spot: Spot price, price of the underlying at t
+        strike: Strike price, exercise price of option at T
+
+    sigma, time_to_maturity and rf_rate should be on the same time scale,
+    e.g. annualized.
+    """
     d1 = bs_d1(sigma, time_to_maturity, rf_rate, spot, strike)
     d2 = bs_d2(sigma, time_to_maturity, rf_rate, spot, strike) # Note: d1 calc made twice
     return norm.cdf(d1) * spot - norm.cdf(d2) * pv(strike, rf_rate, time_to_maturity)
 
 def european_put_value(sigma, time_to_maturity, rf_rate, spot, strike):
+    """Put option price with put-call parity.
+
+    Refer to european_call_value.
+    """
     call = european_call_value(sigma, time_to_maturity, rf_rate, spot, strike)
     return call - spot + strike * pv(1, rf_rate, time_to_maturity)
